c6029ccc7faf3ff8a9fc300f9fc0703cae5b8861
TUBATI JAHNAVI <322114611054@andhrauniversity.edu.in>
2024-10-08 00:37:52 -0700

    AI Nutritionist App

---
+++ chat.py
@@ -0,0 +1,48 @@
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+"""
+
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+# Create the model
+generation_config = {
+  "temperature": 0,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+history = []
+
+print("Bot: Hello, ho can i help you?")
+
+while True: 
+
+    user_input = input("You: ")
+  
+    chat_session = model.start_chat(
+        history=history    
+    )
+
+    response = chat_session.send_message(user_input)
+
+    model_response = response.text
+
+    print(f'Bot: {model_response}')
+    print()
+
+    history.append({"role": "user", "parts": [user_input]})
+    history.append({"role": "model", "parts": [model_response]})
